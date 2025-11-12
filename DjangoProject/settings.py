44520c4cfe5ee2de7e75e3818e1584a04d313d2d
odreos <youri.snchz@eduge.ch>
2025-11-12 16:23:24 +0100

    prep deploy: settings, static, requirements

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8)$#87#iqnnyv$f)a6nj)d#%znx(!(ha**hd(6bs0uhhx30@21'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ossydreos.pythonanywhere.com"]
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
